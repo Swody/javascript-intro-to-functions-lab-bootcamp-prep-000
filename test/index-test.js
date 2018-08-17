9d1f9deeda96c957d592b7969340d0c4352fbb17
Gavin Hemsley <gaivo2@hotmail.com>
2018-08-17 17:17:46 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 
 describe('whisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(shout('hello')).toEqual('hello')
   })
 })
 
