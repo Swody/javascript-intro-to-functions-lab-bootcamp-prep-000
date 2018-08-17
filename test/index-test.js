18688d6e23ce6169a310655b00f222decfc92da4
Gavin Hemsley <gaivo2@hotmail.com>
2018-08-17 18:41:36 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -37,7 +37,7 @@ describe('logWhisper(string)', function() {
 
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I CAN\'T HEAR YOU!")
+    expect(sayHiToGrandma('hello')).toEqual("I can\'t hear you!")
   })
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
