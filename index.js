c6f3152498cf66b3d9628c20ae9cb51483ec7a13
Gavin Hemsley <gaivo2@hotmail.com>
2018-08-17 18:33:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string)
-var 'hello' = 
+var 'hello'= 
 return 'I can\'t hear you!'
 function sayHiToGrandma(string)
 return 'YES INDEED!'
